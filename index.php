415564dc0f6eb45c9c2bd824b619c63133f37581
The_Smashers_Design <71424660+The-Smashers-Design@users.noreply.github.com>
2021-01-04 16:45:53 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> 
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+  </head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand text-warning a2" href="#">Programing</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsenavbar">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="collapsenavbar">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            
+              <li class="nav-item ">
+                <a class="nav-link active a3"  href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active a3"  href="#">Service</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active a3"  href="#">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active a3"  href="#">Contact-Us</a>
+              </li>
+            
+            <form class="d-flex a1">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-warning" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+
+      <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"></li>
+          <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"></li>
+          <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="IMG/s1.jpg" class=" w-100 a4" alt="...">
+            <div class="carousel-caption d-none d-md-block a5">
+              <h5>First</h5>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="IMG/s2.jpg" class=" w-100a a4" alt="...">
+            <div class="carousel-caption d-none d-md-block a6">
+              <h5>Second </h5>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="IMG/s3.jpg" class=" w-100 a4" alt="...">
+            <div class="carousel-caption d-none d-md-block a5">
+              <h5>Third slide</h5>
+              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            </div>
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </a>
+      </div>
+      <div class="a10">
+      <div class="container a7">
+        <div class="a8">
+          <h1>Associated Partners</h1>
+        </div>
+        <div class="row mt-5">
+          <div class="col-lg-4 col-md-4 col-sm-12 col-10 d-block m-auto">
+              <div class="card">
+                  <img src="IMG/H4.jpg" class="card-img img-fluid">
+                  
+              </div>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-12 col-10 d-block m-auto">
+              <div class="card">
+                  <img src="IMG/H3.jpg" class="card-img img-fluid">
+                  
+              </div>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-12 col-10 d-block m-auto">
+              <div class="card">
+                  <img src="IMG/H6.jpg" class="card-img img-fluid">
+                  
+              </div>
+          </div>
+
+          <div class="row mt-5">
+            <div class="col-lg-4 col-md-4 col-sm-12 col-10 d-block m-auto">
+                <div class="card">
+                    <img src="IMG/H7.jpg" class="card-img img-fluid">
+                    
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-12 col-10 d-block m-auto">
+                <div class="card">
+                    <img src="IMG/H9.jpg" class="card-img img-fluid">
+                    
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-12 col-10 d-block m-auto">
+                <div class="card">
+                    <img src="IMG/H13.jpg" class="card-img img-fluid">
+                    
+                </div>
+            </div>
+            </div>
+          
+      </div>
+      </div>
+      <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $phone=$_POST['phone'];
+        $msg=$_POST['msg'];
+        /*$desc = $_POST['desc'];*/
+        
+      
+      // Connecting to the Database//
+      $servername = "localhost";
+      $username = "root";
+      $password = "";
+      $database = "code";
+
+      // Create a connection
+      $conn = mysqli_connect($servername, $username, $password, $database);
+      // Die if connection was not successful
+      if (!$conn){
+          die("Sorry we failed to connect: ". mysqli_connect_error());
+      }
+      else{ 
+        // Submit these to a database
+        // Sql query to be executed 
+        $sql="INSERT INTO `data1` ( `name`, `email`, `phone`, `msg`) VALUES ( '$name', '$email','$phone','$msg')";
+        $result = mysqli_query($conn, $sql);
+ 
+        if($result){
+          echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+          <strong>Success!</strong> Your entry has been submitted successfully!
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>';
+        }
+        else{
+            // echo "The record was not inserted successfully because of this error ---> ". mysqli_error($conn);
+            echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+          <strong>Error!</strong> We are facing some technical issue and your entry ws not submitted successfully! We regret the inconvinience caused!
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>';
+        }
+
+      }
+
+    }
+
+    
+?>
+
+      <div class=" container a11">
+         <div class="row">
+           <div class="col-md-6 text -left pad-10 animated fadeInLeft visible" data-animation="fadeInLeft">
+        <h2>About-Us</h2>
+        <p align="justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Excepturi vel sapiente dicta ab eaque temporibkkus.Quod, id sed. 
+          Dolores, soluta ex! Laborum ad perspiciatis vero a. Sunt minus molestias itaque.</p>
+
+    <p align="justify">Lorem ipsum dolor sit amet consectetur adipisicing elit 
+          Excepturi vel sapiente dicta ab eaque temporibkkus. Quod, id sed. 
+          Dolores, soluta ex! Laborum ad perspiciatis vero a. Sunt minus molestias itaque.</p>
+      
+          <p align="justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Excepturi vel sapiente dicta ab eaque temporibkkus. Quod, id sed. 
+            Dolores, soluta ex! Laborum ad perspiciatis vero a. Sunt minus molestias itaque.</p>
+      </div>
+      <div class="col-md-6 text -left pad-10 animated fadeInLeft visible o1" data-animation="fadeInLeft">
+        <h2>Get In Touch</h2>
+        <form action="index.php" method="POST">
+        <input type="text" class="form-control" name="name"  placeholder="Name"  aria-label="First name">
+        <input type="text" class="form-control mt-4" name="email" placeholder="Email"  aria-label="First name">
+        <input type="text" class="form-control  mt-4"name="phone" placeholder="Phone_No"  aria-label="First name">
+        <textarea class="form-control mt-4" id="exampleFormControlTextarea1" name="msg" placeholder="Message" rows="3"></textarea>
+        <button type="submit" class="btn btn-success mt-3">submit</button>
+      </div>
+      </div>
+      </div>
+    </form> 
+      
+      </div>
+      <div class="a15">
+        
+          <div class="bg-dark"></div>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30703867.071679905!2d64.40183608457191!3d20.049158955693056!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30635ff06b92b791%3A0xd78c4fa1854213a6!2sIndia!5e0!3m2!1sen!2sin!4v1609692740921!5m2!1sen!2sin" width="100%" height="500" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+          </div>
+        </div>
+      
+      <div class="m1">
+      <div class="container f1">
+        <div class="row">
+        <div class="col-md-10 mt-4">
+          <h6>CopyRight @2021 By TSD</h6>
+        </div>
+        <div class="col-md-2 mt-5">
+      <div class="imgsetting d-block text-white ">
+            <a href="#"><i class="fa fa-instagram fa-2x m2 text-white"></i></a>
+            <a href="#"><i class="fa fa-facebook-official fa-2x m2 text-white"></i></a>
+            <a href="#"><i class="fa fa-linkedin-square  fa-2x m2 text-white"></i></a>
+            
+          </div>
+        </div>
+        </div>
+        </div>
+        </div>
+
+         <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+
+      
+    
+</body>
+</html>
